10cee3a2602bc30442dc96bf629e3de989404584
lihaochen <lihaochen202@outlook.com>
2023-05-22 13:27:23 +0800

    feat: allow passing null value when parsing the title

--- composables/seo.ts
+++ composables/seo.ts
@@ -1,9 +1,13 @@
 import type { MaybeRefOrGetter } from 'vue'
 
-export function useHeadTitle(name?: MaybeRefOrGetter<string>) {
+export function useHeadTitle(name?: MaybeRefOrGetter<string | null | undefined>) {
   const appConfig = useAppConfig()
-  const title = computed(() =>
-    name ? `${appConfig.title} | ${toValue(name)}` : appConfig.title,
-  )
+  const title = computed(() => {
+    if (!name)
+      return appConfig.title
+
+    const _name = toValue(name)
+    return _name ? `${appConfig.title} | ${_name}` : appConfig.title
+  })
   return title
 }
