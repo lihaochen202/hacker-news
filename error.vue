2514b55ced1cca742a3ff865253bf814f262dd0f
lihaochen <lihaochen202@outlook.com>
2023-05-23 10:26:04 +0800

    feat: add error page

---
+++ error.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import DefaultLayout from '~/layouts/default.vue'
+
+useHead({
+  title: useHeadTitle('Error'),
+})
+</script>
+
+<template>
+  <div>
+    <DefaultLayout>
+      <div min-h="lg sm:2xl" rounded bg-white flex="~ col justify-center items-center">
+        <p mb-10 text-gray-600>
+          Oops! Something went wrong.
+        </p>
+        <ButtonBack />
+      </div>
+    </DefaultLayout>
+  </div>
+</template>
