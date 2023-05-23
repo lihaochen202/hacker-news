ef28998a52ffa5f3c04bea57250236487a6cab81
lihaochen <lihaochen202@outlook.com>
2023-05-23 09:38:47 +0800

    refactor: remove redundant style attributes

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,18 +19,18 @@ const appConfig = useAppConfig()
   <header flex="~ justify-center" bg-slate-950 font-300 text-white>
     <nav
       flex="~ items-center" h-14
-      w="full lg:250 xl:250 2xl:250"
-      p="x-5 lg:x-0 xl:x-0 2xl:x-0"
+      w="full lg:250"
+      p="x-5 lg:x-0"
     >
       <NuxtLink
         flex="shrink-0"
-        m="r-6 sm:r-8 md:r-8 lg:r-8 xl:r-8 2xl:r-8"
+        m="r-6 sm:r-8"
         to="/"
       >
         <img h-7 w-7 src="/logo.svg" alt="Logo">
       </NuxtLink>
 
-      <ul flex gap="5 sm:7 md:7 lg:7 xl:7 2xl:7">
+      <ul flex gap="5 sm:7">
         <li v-for="item in navigations" :key="item.name">
           <NuxtLink
             link
@@ -43,13 +43,13 @@ const appConfig = useAppConfig()
       </ul>
 
       <a m-l-auto :href="appConfig.repo" target="_blank" rel="noopener noreferrer">
-        <i block="sm:~ md:~ lg:~ xl:~ 2xl:~" text-6 class="i-ri:github-fill" />
+        <i block="sm:~" text-6 class="i-ri:github-fill" />
       </a>
     </nav>
   </header>
 
-  <main flex="~ justify-center" p="x-5 lg:x-0 xl:x-0 2xl:x-0">
-    <div py-4 w="full lg:250 xl:250 2xl:250">
+  <main flex="~ justify-center" p="x-5 lg:x-0">
+    <div py-4 w="full lg:250">
       <slot />
     </div>
   </main>
