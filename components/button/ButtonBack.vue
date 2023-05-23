63e42ec12307d6999261de21018ec36bf4b3c691
lihaochen <lihaochen202@outlook.com>
2023-05-23 10:25:44 +0800

    feat: enrich 404 page

---
+++ components/button/ButtonBack.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    text="white"
+    rounded
+    bg-sky-600
+    transition
+    hover:opacity-80
+  >
+    <NuxtLink block p="x-5 y-2" to="/">
+      Go Back Home
+    </NuxtLink>
+  </button>
+</template>
--- pages/404.vue
+++ pages/404.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  title: useHeadTitle('404'),
+})
+</script>
 
 <template>
-  <div>404</div>
+  <div min-h="lg sm:2xl" rounded bg-white flex="~ col justify-center items-center">
+    <div flex skew-x--12 text-5xl>
+      <div class="i-ri-number-4" />
+      <div class="i-ri-number-0" />
+      <div class="i-ri-number-4" />
+    </div>
+    <p my-10 text-gray-600>
+      The page you are looking for can't be found.
+    </p>
+    <ButtonBack />
+  </div>
 </template>
